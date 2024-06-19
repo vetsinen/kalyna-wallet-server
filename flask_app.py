590bda5804f852ad71320fda379199e4635ee9f2
vetsinen <vetsinen@gmail.com>
2024-06-20 01:33:40 +0300

    enters user phrase wallet and logouts

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,42 @@
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask, render_template
+from kalyna_wallet_lib import generate_cusom_mnemo, get_solana_derived_wallet, get_sol_balance
+from flask import Flask, render_template, request, session, redirect, url_for, jsonify
 
 app = Flask(__name__, static_folder='static')
+app.secret_key = 'KALYNA_WALLET_214_SECRET_KEY'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if not 'words' in session:
+        # session['wallet'] = "7tez6hV2Ju1FkTTuFh3TA3fFAQvrKLUB9o1PQtDzvrBt"
+        return render_template('login.html')
+    else:
+        print(session['words'])
+        words = session['words']
+        address = get_solana_derived_wallet(words)[0]
+        balance = get_sol_balance(address)
+        wallet = {
+            'address': address,
+            'balance': balance
+        }
+        return render_template('index.html', wallet=wallet)
+
+@app.post('/set_wallet')
+def set_wallet():
+        # Save the form data to the session object
+        data = request.get_json()  # Get JSON data from request body
+        text = data.get('text')
+        print(f'Received content: {text}')
+        mnemo = generate_cusom_mnemo(text)
+        if not mnemo:
+            return jsonify({'status':'error','message': 'text is to short'})
+        session['words'] = mnemo
+        return jsonify({'status':'ok','message': 'text processe successfully'})
+
+@app.route('/logout')
+def logout():
+    # Clear words stored in the session object
+    session.pop('words', default=None)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
